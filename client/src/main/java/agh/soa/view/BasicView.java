379674068ba1591b495714b64925e7544cd4b0a3
SAMSUNG <juliusz.rylko@gmail.com>
2019-06-03 21:12:28 +0200

    Added exceptions to ticket buyings, also TicketsService now have method getMostRecentTicket()

--- client/src/main/java/agh/soa/view/BasicView.java
+++ client/src/main/java/agh/soa/view/BasicView.java
@@ -1,7 +1,9 @@
 package agh.soa.view;
 
+import agh.soa.dto.TicketDTO;
 import agh.soa.service.IParkometersService;
 import agh.soa.model.User;
+import agh.soa.service.ITicketsService;
 import agh.soa.service.IUsersService;
 import lombok.Data;
 
@@ -16,10 +18,18 @@ public class BasicView {
     @EJB(lookup = "java:global/implementation-1.0-SNAPSHOT/UsersService")
     IUsersService usersService;
 
+    @EJB(lookup = "java:global/implementation-1.0-SNAPSHOT/TicketsService")
+    ITicketsService ticketsService;
+
     public List<User> getUsers(){
         return usersService.getUsers();
     }
 
+    public TicketDTO getMostRecentTicket(){
+        TicketDTO result = ticketsService.getMostRecentTicket();
+        System.out.println(result);
+        return result;
+    }
 
 
 
